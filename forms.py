2b26644acf7495764e77cb5e11a5f1cdbd1ce0a7
John Chong <johnchong.99.17@gmail.com>
2023-05-03 14:09:41 -0500

    added EditProfileForm

--- forms.py
+++ forms.py
@@ -47,5 +47,42 @@ class LoginForm(FlaskForm):
     )
 
 
+class EditProfileForm(FlaskForm):
+    """User edit form."""
+
+    username = StringField(
+        'Username',
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+    email = StringField(
+        'E-mail',
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+
+    password = PasswordField(
+        'Password',
+        validators=[InputRequired(), Length(min=6, max=50)],
+    )
+
+    image_url = StringField(
+        '(Optional) Image URL',
+        validators=[Optional(), URL(), Length(max=255)]
+    )
+
+    location = StringField(
+        'Location',
+        validators=[Length(max=30)]
+    )
+
+    bio = TextAreaField(
+        'Bio'
+    )
+
+    header_image_url = StringField(
+        '(Optional) Header Image URL',
+        validators=[Optional(), URL(), Length(max=255)]
+    )
+
 class CSRFProtectForm(FlaskForm):
     """CSRF Protection Form for protection"""
