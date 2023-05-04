06a62a132db68d403d15decf6a4e631c3c329192
John Chong <johnchong.99.17@gmail.com>
2023-05-04 13:48:31 -0500

    post code review

--- app.py
+++ app.py
@@ -396,13 +396,13 @@ def homepage():
 # LIKE/UNLIKE REQUESTS
 
 @app.post('/like/<int:msg_id>')
-def like(msg_id):
+def like_msg(msg_id):
     """Add LikeMessage Instance to DB for current user"""
-
+    #TODO: GUARD
     if not g.user or g.csrf_form.validate_on_submit():
         flash("Access unauthorized.", "danger")
         return redirect("/")
-    else:
+    else:#TODO: no need for "Else same with unlike_msg, show_likes"
         url = request.form["url"] or "/"
         message = Message.query.get(msg_id)
         g.user.liked_messages.append(message)
@@ -412,7 +412,7 @@ def like(msg_id):
         return redirect(url)
 
 @app.post('/unlike/<int:msg_id>')
-def unlike(msg_id):
+def unlike_msg(msg_id):
     """Add LikeMessage Instance to DB for current user"""
 
     if not g.user or g.csrf_form.validate_on_submit():
