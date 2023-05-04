eed528feb64495180cb568c08fc7cd428042fbc9
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-05-04 16:24:54 -0700

    add test cases for is_following, is_followed_by, and User.signup

--- test_user_model.py
+++ test_user_model.py
@@ -2,13 +2,15 @@
 
 # run these tests like:
 #
-#    python -m unittest test_user_model.py
+#    FLASK_DEBUG=False python -m unittest test_user_model.py
 
 
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follow
+from models import db, User, Message, Follow, DEFAULT_IMAGE_URL
+
+from sqlalchemy.exc import IntegrityError
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -46,8 +48,74 @@ class UserModelTestCase(TestCase):
         db.session.rollback()
 
     def test_user_model(self):
+        """Does the user have 0 messages and 0 followers?"""
+
         u1 = User.query.get(self.u1_id)
 
-        # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+
+    def test_is_following(self):
+        """Is user 1 following user 2?"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        u2.followers.append(u1)
+
+        self.assertEqual(u1.is_following(u2), True)
+
+    def test_is_not_following(self):
+        """Is user 1 not following user 2"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        self.assertEqual(u1.is_following(u2), False)
+
+
+    def test_is_followed_by(self):
+        """Is user 2 followed by user 1?"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        u2.followers.append(u1)
+
+        self.assertEqual(u2.is_followed_by(u1), True)
+
+    def test_is_not_followed_by(self):
+        """Is user 2 not followed by user 1?"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        self.assertEqual(u2.is_followed_by(u1), False)
+
+    def test_user_signup_success(self):
+        """
+        Does the User signup successfully create a new user given
+        valid credentials?
+        """
+
+        user = User.signup("user_success",
+                           "user_success@email.com",
+                           "password",
+                           None)
+        db.session.commit()
+
+        self.assertEqual(user.username, "user_success")
+        self.assertEqual(user.email, "user_success@email.com")
+        self.assertEqual(user.image_url, DEFAULT_IMAGE_URL)
+        self.assertTrue(user.password.startswith("$2b$12$"))
+
+
+    def test_user_signup_fail(self):
+        """
+        Does the User signup successfully create a new user given
+        valid credentials?
+        """
+
+        user = User.signup("u2", "u2@email.com", "password", None)
+
+        self.assertRaises(IntegrityError, db.session.commit)
